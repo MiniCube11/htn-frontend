8cf5a64a6790f691cf8f3be3f273dde3bcb9afd1
Ching Lam Lau <contact.minicube11@gmail.com>
2025-02-05 21:57:13 -0500

    feat: format date and time

--- components/Event.tsx
+++ components/Event.tsx
@@ -1,10 +1,18 @@
 import { TEvent } from "@/lib/type";
+import { formatDate, formatTime } from "@/lib/utils";
 
 export default function Event({ event }: { event: TEvent }) {
+  const { month, day } = formatDate(event.start_time);
+  const startTime = formatTime(event.start_time);
+  const endTime = formatTime(event.end_time);
+
   return (
     <div className="border p-4 my-4 hover:bg-gray-50 transition">
         <h2>{event.name}</h2>
-        <p>{event.start_time}</p>
+        <p>{month}</p>
+        <p>{day}</p>
+        <p>{startTime}</p>
+        <p>{endTime}</p>
     </div>
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,14 +4,18 @@ export async function getEvents() : Promise<TEvent[]> {
   const events = await fetch("https://api.hackthenorth.com/v3/events");
   const eventsData = await events.json();
 
-  for (const event of eventsData) {
-    event.start_time = formatTime(event.start_time);
-    event.end_time = formatTime(event.end_time);
-  }
-
   return eventsData;
 }
 
-function formatTime(time: number) {
-  return new Date(time).toLocaleString();
+export function formatDate(time: number) {
+  const date = new Date(time);
+
+  return {
+    month: date.toLocaleString("default", { month: "short" }),
+    day: date.getDate(),
+  }
+}
+
+export function formatTime(time: number) {
+  return new Date(time).toLocaleTimeString("en-us", { hour: "numeric", minute: "2-digit", hour12: true });
 }
