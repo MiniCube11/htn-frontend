d2541d9bb19b030fd034183f4074f3f3dd428f21
Ching Lam Lau <contact.minicube11@gmail.com>
2025-02-11 11:09:59 -0500

    feat: create login system using local session

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (Cookies.get("username")) {
+      router.push("/");
+    }
+  }, []);
+
+  const handleLogin = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    if (username === "hacker" && password === "htn2025") {
+      Cookies.set("username", username, {expires: 1});
+      router.push("/");
+    } else {
+      setError("Invalid username or password. Try using 'htn2025' as the password.");
+    } 
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen lg:bg-[#F3F5FC]">
       <form className="w-screen lg:w-[600px] py-16 bg-white lg:border lg:border-gray-300 rounded-lg flex flex-col justify-center items-center">
@@ -9,12 +37,14 @@ export default function Login() {
           type="text"
           placeholder="hacker"
           className="w-[90%] lg:w-[80%] py-2 px-4 border border-gray-500 focus:border-gray-800 rounded-md outline-none mt-2"
+          onChange={(e) => setUsername(e.target.value)}
         />
         <h2 className="mt-6 text-gray-800 w-[90%] lg:w-[80%]">Password</h2>
         <input
           type="password"
           placeholder="•••••••••••"
           className="w-[90%] lg:w-[80%] py-2 px-4 border border-gray-500 focus:border-gray-800 rounded-md outline-none mt-2"
+          onChange={(e) => setPassword(e.target.value)}
         />
         <div className="flex justify-between items-center mt-6 w-[90%] lg:w-[80%]">
           <div className="flex items-center">
@@ -23,7 +53,10 @@ export default function Login() {
           </div>
           <a href="#" className="text-gray-800 hover:text-gray-600 transition block underline">Forgot password?</a>
         </div>
-        <button className="bg-[#7790F3] hover:bg-[#8FA3F5] transition block w-[90%] lg:w-[80%] rounded-md py-2 mt-12 text-white font-semibold">
+        {error && <p className="text-red-500 text-sm mt-4 w-[90%] lg:w-[80%]">{error}</p>}
+        <button
+          onClick={handleLogin}
+          className="bg-[#7790F3] hover:bg-[#8FA3F5] transition block w-[90%] lg:w-[80%] rounded-md py-2 mt-12 text-white font-semibold">
           Login
         </button>
       </form>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,37 @@
+"use client";
+
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
 import { getEvents } from "../lib/utils";
 import Event from "../components/Event";
+import { TEvent } from "@/lib/type";
 
-export default async function Home() {
-  const events = await getEvents();
+export default function Home() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const [events, setEvents] = useState<TEvent[]>([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const events = await getEvents();
+      setEvents(events);
+    };
+
+    fetchEvents();
+
+    if (Cookies.get("username")) {
+      router.push("/");
+    }
+
+    setLoading(false);
+  }, []);
 
   return (
     <div className="flex flex-col items-center w-full mb-32">
       <h2>Events</h2>
+      {loading && <p>Loading...</p>}
       {events.map((event) => (
         <Event event={event} />
       ))}
