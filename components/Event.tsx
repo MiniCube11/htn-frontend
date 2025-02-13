9f83301293efa94fbd198f5711f257fffa05c999
Ching Lam Lau <contact.minicube11@gmail.com>
2025-02-12 19:14:25 -0500

    feat: make events page more colourful

--- components/Event.tsx
+++ components/Event.tsx
@@ -6,22 +6,26 @@ export default function Event({ event, openEvent }: { event: TEvent; openEvent: 
   const startTime = formatTime(event.start_time);
   const endTime = formatTime(event.end_time);
   const eventType = formatEventType(event.event_type);
+  const bgColour = eventType === "Workshop" ? "bg-[#ABEDD7]" : eventType === "Activity" ? "bg-[#EBD2F0]" : "bg-[#BCCAF6]";
+  const textColour = eventType === "Workshop" ? "text-[#125b41]" : eventType === "Activity" ? "text-[#78228c]" : "text-[#213789]";
 
   return (
     <div key={event.id}
       onClick={() => openEvent(event.id)}
-      className="w-[90vw] lg:w-[600px] border border-gray-200 rounded py-4 px-6 my-2 hover:bg-gray-50 transition">
+      className="relative w-[90vw] lg:w-[600px] border border-gray-200 rounded py-4 px-10 my-3 hover:bg-gray-50 transition">
+      <div className={`absolute rounded-l left-0 top-0 w-[5px] h-full ${bgColour}`}></div>
       <h2 className="font-medium">{event.name}</h2>
       <div className="block lg:flex text-gray-700 text-sm mt-1">
-        <p className="mr-5">{month} {day} {startTime} - {endTime}</p>
+        <p className="mr-5">📆 {month} {day} {startTime} - {endTime}</p>
         {event.speakers && event.speakers.length > 0 &&
-          <p>{event.speakers[0].name}</p>
+          <p>🗣️ {event.speakers[0].name}</p>
         }
       </div>
-      <span className="text-xs mt-2 text-gray-700 border border-gray-300 inline-block px-1 py-0.5 rounded-md">
+      <span
+        className={`text-xs mt-2 ${textColour} inline-block px-1.5 py-0.5 rounded-md ${bgColour}`}>
         {eventType}
       </span>
-      <span className="text-xs mt-2 ml-2 text-gray-700 border border-gray-300 inline-block px-1 py-0.5 rounded-md">
+      <span className="text-xs mt-2 ml-2 text-[#1f474c] border bg-[#ceeff3] inline-block px-1.5 py-0.5 rounded-md">
         {event.permission === "public" ? "Public" : "Private"}
       </span>
     </div>
